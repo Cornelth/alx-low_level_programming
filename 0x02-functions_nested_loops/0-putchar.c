9cea0b00f1c36154a9e040abf049ae89c6cb8c3f
Cornelth <cornelt.elect@yahoo.com>
2023-02-21 15:01:03 +0100

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	/*
+	 * main - To print using puts function
+	 *
+	 *
+	 *Return: 0
+	 */
+	puts("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Task on Functions and Nestled loops
