4584a6f498981247a1378a57f5d8357a6cea9b41
Cornelth <cornelt.elect@yahoo.com>
2023-02-22 01:43:02 +0100

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,9 +14,11 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			_putchar (i);
+			_putchar (i / 10);
+			_putchar (i % 10);
 			_putchar (':');
-			_putchar (j);
+			_putchar (j / 10);
+			_putchar (j % 10);
 			_putchar ('\n');
 		}
 	}
