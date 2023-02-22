002e7da2f9122c42facf209fd94eb91952957a8c
Cornelth <cornelt.elect@yahoo.com>
2023-02-22 01:32:30 +0100

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *jack_bauer - this main prints every minute
+ *
+ *
+ *Return: nothing
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar (i);
+			_putchar (':');
+			_putchar (j);
+			_putchar ('\n');
+		}
+	}
+}
