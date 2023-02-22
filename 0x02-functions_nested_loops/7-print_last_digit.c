85e0aed129024b37df7d69a99744506262af69db
Cornelth <cornelt.elect@yahoo.com>
2023-02-22 01:12:39 +0100

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,19 @@
  */
 int print_last_digit(int n)
 {
-	_putchar(n % 10);
+int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar ('0' + last_digit);
+		return (last_digit);
+	}
+
+	else
+	{
+		last_digit = (n % 10);
+		_putchar ('0' + last_digit);
+		return (last_digit);
+	}
 }
