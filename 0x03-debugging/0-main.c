95b833538eb2de0d42f1989214d6c12bbff36e9a
Cornelth <cornelt.elect@yahoo.com>
2023-02-23 20:27:15 +0100

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main -this function  tests function that prints if
+* integer is positive or negative
+* Return: nothing
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
