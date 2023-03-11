e82b01570e5bdf53f9cab758a9c692e568d2140c
Cornelth <cornelt.elect@yahoo.com>
2023-03-11 07:44:01 +0100

    TASK 0X09

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,11 +1,10 @@
-#include "main.h"
+#include "unistd.h"
 /**
- * _putchar - function to print a character
+ * _putchar - function to print and write a character
  * @c: Character to print
- * Return: 0
+ * Return: 1 for success, -1 for error
  **/
 int _putchar(char c)
 {
-	_putchar('c');
-	return (0);
+	return (write(1, &c, 1));
 }
