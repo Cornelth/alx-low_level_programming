0c606f50727fe4210b8550eabda749189a6c20f9
Cornelth <cornelt.elect@yahoo.com>
2023-03-14 21:03:34 +0100

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ *create_array - a function to create an array of char
+ *@size: number elements array
+ *@c: char
+ *Return: pointer if success, 0 if fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *buffer;
+unsigned int position;
+
+if (size == 0)
+{
+return (NULL);
+}
+
+/*initializing and defining a dynmem*/
+dynmemo = (char *) malloc(size * sizeof(c));
+
+if (dynmemo == 0)
+{
+return (NULL);
+}
+
+else
+{
+position = 0;
+while (position < size) /*While for array*/
+{
+*(dynmemo + position) = c;
+position++;
+}
+
+return (dynmemo);
+}
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+** 0x0B. C - malloc, free
+* Comprises of 5 madatory tasks and 2 optional tasks
