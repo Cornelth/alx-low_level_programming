992cabb884c0b2d865b524d8859674daa7d404a2
Cornelth <cornelt.elect@yahoo.com>
2023-03-17 20:44:54 +0100

    Advanced task 2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,20 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
- * _atoi_charr - converts char to integer.
- * @x: character to convert.
- * Return: integer.
- **/
-
-int _atoi_charr(char x)
-{
-	unsigned int res;
-
-	if (x <= '9' && x >= '0')
-		res = x - '0';
-	return (res);
-}
 
 /**
  * _is_a_number - Define if a string is a number.
