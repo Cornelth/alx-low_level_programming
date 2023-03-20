11a41fffe4e79488ca03f23da26a6e68dc14d341
Cornelth <cornelt.elect@yahoo.com>
2023-03-20 23:08:13 +0100

    Task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+** 0x0E. C - Structures, typedef **
+         *March 20th 2023*
+*six compulsory tasks onlt *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - user defined data type structure
+ * @name: atribute pointer to char name in dog
+ * @age: atribute float age in dog
+ * @owner: atribute pointer to char owner in dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
