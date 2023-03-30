baf532fba5f064b60d86e4645cb575c0cc1b3f96
Cornelth <cornelt.elect@yahoo.com>
2023-03-30 21:38:19 +0100

    Task 4

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,11 +7,12 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t element = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        element++;
-    }
-    return (element);
+	size_t element = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		element++;
+	}
+	return (element);
 }
