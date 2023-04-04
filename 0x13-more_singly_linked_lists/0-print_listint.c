6a0ec7d163c44df2d880c8b561be5fd8dcf5bb20
Cornelth <cornelt.elect@yahoo.com>
2023-04-04 22:07:09 +0100

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t list_node = 0;
+	size_t list_node = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        list_node++;
-        h = h->next;
-    }
-
-    return (list_node);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		list_node++;
+		h = h->next;
+	}
+	return (list_node);
 }
