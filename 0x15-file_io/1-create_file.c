d96c3b2fe81142434aa8a628d9f50e6758f80438
Cornelth <cornelt.elect@yahoo.com>
2023-04-10 20:59:12 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (len = 0; text_content[len]; len++)
+			;
 	}
 
 	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wrt = write(o, text_content, len);
+	wrt = write(opn, text_content, len);
 
 	if (opn == -1 || wrt == -1)
 		return (-1);
