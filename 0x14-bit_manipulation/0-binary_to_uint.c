bdff57cc2819d0c139e50bf27bf0e9079192a7aa
Cornelth <cornelt.elect@yahoo.com>
2023-04-10 22:37:09 +0100

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int numb = 0;
 	int len = 0;
 
-	if (b[len] == NULL)
+	if (b[len] == '\0')
 		return (0);
 
 	while ((b[len] == '0') || (b[len] == '1'))
