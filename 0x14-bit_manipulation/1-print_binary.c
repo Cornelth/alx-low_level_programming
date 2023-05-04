1de23f2218bd56869714012a938fac79932a21ef
Cornelth <cornelt.elect@yahoo.com>
2023-05-04 22:05:04 +0100

    Printing bin rep

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,10 +3,11 @@
 /**
 * print_binary -  fn that Prints the bin rep of a number.
 * @n: The num to be printed in binary.
+* Return: Nothing
 */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
-		 print_binary(n >> 1);
+		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
 }
